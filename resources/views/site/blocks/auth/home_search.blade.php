8d92619891847c6abbca4aa5e06bb29d4b89668d
Hayko <zakhayko@gmail.com>
2019-07-29 21:32:36 +0400

    workplace

--- resources/views/site/blocks/auth/home_search.blade.php
+++ resources/views/site/blocks/auth/home_search.blade.php
@@ -1,7 +1,59 @@
 <div class="home-search">
     <div class="container">
-        <form action="javascript:void(0)" method="post">
-            
-        </form>
+        <div class="home-search-block">
+            <div class="home-search-title">ЗАПЧАСТИ</div>
+            <div class="home-search-content">
+                <div class="home-search-option">Опора двигателя</div>
+                <div class="home-search-option">Опора амортизаторов</div>
+                <div class="home-search-option">Крышка радиатора</div>
+                <div class="home-search-option">Прокладка клапанной крышки</div>
+            </div>
+            <div class="home-search-buttons">
+                <button class="home-search-btn">Еще</button>
+            </div>
+        </div>
+        <div class="home-search-block">
+            <div class="home-search-title">БРЕНДЫ</div>
+            <div class="home-search-content">
+                <div class="home-search-option">DEYE China</div>
+                <div class="home-search-option">SEHUN Korea</div>
+                <div class="home-search-option">AGP Malaysia</div>
+                <div class="home-search-option">HDK Japan</div>
+                <div class="home-search-option">BAW Taiwan</div>
+                <div class="home-search-option">CASP Taiwan</div>
+                <div class="home-search-option">ERISTIC Taiwan</div>
+            </div>
+            <div class="home-search-buttons">
+                <button class="home-search-btn">Еще</button>
+            </div>
+        </div>
+        <div class="home-search-block">
+            <div class="home-search-title">МАРКА</div>
+            <div class="home-search-content">
+                <div class="home-search-option">Toyota</div>
+                <div class="home-search-option">Mitsubishi</div>
+                <div class="home-search-option">Nissan</div>
+                <div class="home-search-option">Mazda</div>
+                <div class="home-search-option">Honda</div>
+                <div class="home-search-option">Subaru</div>
+                <div class="home-search-option">Hyundai</div>
+                <div class="home-search-option">Daewoо</div>
+            </div>
+            <div class="home-search-buttons">
+                <button class="home-search-btn">Еще</button>
+            </div>
+        </div>
+        <div class="home-search-block">
+            <div class="home-search-title"><label class="all-inherit" for="home-search-engine">ДВИГАТЕЛЬ</label></div>
+            <div class="home-search-content">
+                <div class="home-search-input">
+                    <input type="text" class="home-search-control" id="home-search-engine">
+                </div>
+            </div>
+            <div class="home-search-buttons">
+                <button class="home-search-btn">Применить</button>
+                <button class="home-search-btn">Сбросить</button>
+            </div>
+        </div>
     </div>
 </div>
